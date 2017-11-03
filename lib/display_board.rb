796917564dab1281f8443a5e4575e752e7c6f866
yurina shin <yurinas@gmail.com>
2017-11-03 17:58:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@ def display_board
   puts "A Tic Tac Toe Board"
 end
 display_board
-puts "   ""   ""   ""
-puts "-----------"
+board = [" "," "," "," "," "," "," "," "," "]
+board = [0]
 puts "    |   |   "#
 puts "-----------"
